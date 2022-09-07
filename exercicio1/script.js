88a207fadc3f2522b9e28f1cdd49c6616216a072
wilsonbregeon <wilsonbregeon@gmail.com>
2022-09-07 17:21:42 -0300

    exercicios a012-condicionais-I resolvidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = 2
 
-if(){
-    console.log("sua mensagem aqui");
+if(num%2 === 0){
+    console.log("PAR");
+}else{
+    console.log("ÍMPAR")
 }
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt("Qual a sua idade?"))
+let TerminouEnsinoMedio = prompt("Terminou o ensino médio?").toLowerCase()
+let CursandoFaculdade = prompt("Está cursando faculdade?").toLowerCase()
 
-if(){
+ if(idade >= 18){
+     console.log(`${idade} anos: Maior de idade`)
+ }else{
+ 	console.log(`${idade} anos: Menor de idade`)
+ }
 
-}else{
-	
-}
+ if(TerminouEnsinoMedio == "sim"){
+     console.log(`${TerminouEnsinoMedio}: Ensino médio completo`)
+ }else{
+     console.log(`${TerminouEnsinoMedio}: Ensino médio incompleto`)
+ }
+
+ if(CursandoFaculdade == "sim"){
+     console.log(`${CursandoFaculdade}: Cursando faculdade`)
+ }else{
+     console.log(`${CursandoFaculdade}: Não está cursando faculdade`)
+ }
+
